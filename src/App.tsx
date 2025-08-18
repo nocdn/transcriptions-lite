fca7fe17766130c262ad32c69896370e3bdb99a8
nocdn <bartek.bak@protonmail.com>
2025-08-18 08:42:49 +0200

    feat: update dependencies, add a favicon, nicer spinner, and toned down some of the blurring

--- src/App.tsx
+++ src/App.tsx
@@ -1,91 +1,92 @@
-import { useState } from "react";
-import DropZone from "./DropZone";
-import { AudioLines, LoaderCircle, X } from "lucide-react";
+import { useState } from "react"
+import DropZone from "./DropZone"
+import { LoaderCircle, X } from "lucide-react"
+import AudioIcon from "./components/icons/audio"
+import Spinner from "./components/Spinner"
+
+// take the api url from the .env.local
+const API_URL = import.meta.env.VITE_BACKEND_URL
 
 function App() {
-  const [stage, setStage] = useState("initial");
-  const [transcriptionText, setTranscriptionText] = useState("");
-  const [copiedMessage, setCopiedMessage] = useState(false);
+  const [stage, setStage] = useState("initial")
+  const [transcriptionText, setTranscriptionText] = useState("")
+  const [copiedMessage, setCopiedMessage] = useState(false)
 
   function handleClick() {
     // open a file dialog here and select one file
-    const input = document.createElement("input"); // create an input element
-    input.type = "file"; // set the input type to file
-    input.accept = "audio/*"; // specify accepted file types to audio
+    const input = document.createElement("input") // create an input element
+    input.type = "file" // set the input type to file
+    input.accept = "audio/*" // specify accepted file types to audio
     input.onchange = (event: any) => {
       // attach an event listener
-      const file = event.target.files?.[0]; // get the selected file
+      const file = event.target.files?.[0] // get the selected file
       if (file) {
         // check if a file was selected
-        handleFile(file); // call handleFile with the selected file
+        handleFile(file) // call handleFile with the selected file
       }
-    };
+    }
     if (transcriptionText !== "") {
-      return;
+      return
     } else {
-      input.click(); // trigger the file dialog
+      input.click() // trigger the file dialog
     }
   }
 
   async function handleFile(file: any) {
-    console.log(file);
-    setStage("processing");
-    setCopiedMessage(false);
-    const formData = new FormData();
-    formData.append("file", file);
-    formData.append("fireworksModelValue", "whisper-v3");
-    formData.append("currentModelProvider", "fireworks");
+    console.log(file)
+    setStage("processing")
+    setCopiedMessage(false)
+    const formData = new FormData()
+    formData.append("file", file)
+    formData.append("fireworksModelValue", "whisper-v3")
+    formData.append("currentModelProvider", "fireworks")
 
-    const response = await fetch(
-      "https://whisperapi.bartoszbak.org/api/upload",
-      {
-        method: "POST",
-        body: formData,
-      }
-    );
+    const response = await fetch(`${API_URL}/api/upload`, {
+      method: "POST",
+      body: formData,
+    })
 
     if (response.status === 429) {
-      console.log("rate limited");
-      return;
+      console.log("rate limited")
+      return
     }
 
-    const data = await response.json();
-    console.log(data.transcription);
-    navigator.clipboard.writeText(data.transcription);
-    setTranscriptionText(data.transcription);
-    setStage("done");
-    setCopiedMessage(true);
+    const data = await response.json()
+    console.log(data.transcription)
+    navigator.clipboard.writeText(data.transcription)
+    setTranscriptionText(data.transcription)
+    setStage("done")
+    setCopiedMessage(true)
   }
 
   return (
-    <div className="grid place-content-center h-dvh w-screen motion-preset-focus-sm">
+    <div className="grid place-content-center h-dvh w-screen motion-opacity-in-0">
       <DropZone onClick={handleClick} onDropped={handleFile}>
         {(() => {
           // Start of IIFE
           if (stage === "initial") {
             return (
               // Return the JSX for this case
-              <div className="ml-12 mr-12 mt-4 mb-4 flex flex-col items-center gap-1 animate-blur-in-custom">
+              <div className="ml-12 mr-12 mt-4 mb-4 flex flex-col items-center gap-1 motion-opacity-in-0">
                 <div className="w-13 h-13 border border-gray-100 rounded-full grid place-content-center mb-5">
-                  <AudioLines size={18} />
+                  <AudioIcon size={18} />
                 </div>
                 <p className="font-geist text-sm font-medium">
                   Drop an audio file here
                 </p>
-                <p className="opacity-60 text-sm">Max size 100MB</p>
+                <p className="opacity-60 font-jetbrains-mono text-sm">
+                  Max size 100MB
+                </p>
               </div>
-            );
+            )
           } else if (stage === "processing") {
             return (
               // Return the JSX for this case
-              <div className="p-4 flex flex-col items-center text-blue-600 motion-preset-focus">
-                <LoaderCircle
-                  size={18}
-                  className="mb-2 animate-spin opacity-75"
-                />
-                <p className="text-sm font-geist-mono">Processing</p>
+              <div className="p-4 flex flex-col items-center text-blue-600 motion-preset-focus-sm">
+                <Spinner size={18} className="mb-3" />
+                <p className="text-sm font-geist-mono">transcribing</p>
               </div>
-            );
+            )
           } else if (stage === "done") {
             return (
               // Return the JSX for this case
@@ -94,23 +95,24 @@ function App() {
                   {transcriptionText}
                 </p>
               </div>
-            );
+            )
           } else {
-            return null; // Default case
+            return null // Default case
           }
         })()}
       </DropZone>
 
       {copiedMessage ? (
-        <p className="font-jetbrains-mono text-sm font-medium w-full text-center mt-4 text-blue-600/75 animate-copied-message-up inline-flex gap-3 justify-center">
+        <p className="font-jetbrains-mono text-sm font-medium w-full text-center mt-4 text-blue-600/75 animate-copied-message-up inline-flex gap-3 justify-center items-center">
           COPIED{" "}
           <X
             size={18}
-            className="mt-[1px] cursor-pointer"
+            strokeWidth={2.5}
+            className="cursor-pointer bg-gray-100 rounded-full p-1 h-5 w-5 grid place-content-center text-gray-600"
             onClick={() => {
-              setStage("initial");
-              setCopiedMessage(false);
-              setTranscriptionText("");
+              setStage("initial")
+              setCopiedMessage(false)
+              setTranscriptionText("")
             }}
           />
         </p>
@@ -118,7 +120,7 @@ function App() {
         <></>
       )}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
